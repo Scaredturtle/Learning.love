c3db1a2b778790a5e46030312af83aa6935af7eb
Scaredturtle <demarcam2012@yahoo.com>
2017-11-23 20:21:22 -0500

    Hero
    changed hero sprites for ease

--- Main.lua
+++ Main.lua
@@ -3,10 +3,10 @@ local hero
 
 -- Animation Parameters --
 local fps = 10
-local frame = 2
+local frame = 1
 local anim_timer = 1 / fps
 local anim_xoffset 
-local num_frames = 3
+local num_frames = 6
 
 function love.load()
 	-- Screen Init --
@@ -16,15 +16,15 @@ function love.load()
 	
 	-- Image File Import --
 	love.graphics.setDefaultFilter("nearest", "nearest")
-	hero_sheet = love.graphics.newImage("Assets/MainCharacter.png")
-	hero = love.graphics.newQuad(1, 1, 25, 29, hero_sheet:getDimensions())
+	hero_sheet = love.graphics.newImage("Assets/Oldhero.png")
+	hero = love.graphics.newQuad(1, 1, 16, 16, hero_sheet:getDimensions())
 
 	-- Player Init --
 	player = {}
 	player.x = 25
 	player.y = 25
-	player.width = 12
-	player.height = 14
+	player.width = 8
+	player.height = 8
 	player.speed = 1.5
 	player.swing = function()
 	
@@ -39,6 +39,15 @@ function love.update(dt)
 		player.x = player.x + player.speed
 
 		-- Walking Right Animation --
+
+		anim_timer = anim_timer * dt
+		if anim_timer <= 0 then
+			anim_timer = 1 / fps
+			frame = frame + 1
+			if frame > num_frames then frame = 1 end
+			anim_xoffset = 16 * frame
+			hero:setViewport(anim_xoffset, 16, 16, 16)
+		end
 
 	end
 	if (love.keyboard.isDown("left") and player.x > (0 + player.width)) then
@@ -62,13 +71,13 @@ function love.update(dt)
 			anim_timer = 1 / fps
 			frame = frame + 1
 			if frame > num_frames then frame = 2 end
-			xoffset = 25 * frame
-			hero:setViewport(anim_xoffset, 25)
+			anim_xoffset = 25 * frame
+			hero:setViewport(anim_xoffset, 1, 24, 28)
 		end]]--
 	end
 end
 
 function love.draw()
 	--love.graphics.draw(hero_sheet, 25, 380, 0, 2, 2)
-	--love.graphics.draw(hero_sheet, hero, player.x, player.y, 0, 1.5, 1.5, 12, 14)
+	love.graphics.draw(hero_sheet, hero, player.x, player.y, 0, 1.5, 1.5, 8, 8)
 end
