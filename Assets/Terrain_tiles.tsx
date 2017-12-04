503cdaf36a69f12918630069574718c971b8c6bd
Scaredturtle <demarcam2012@yahoo.com>
2017-12-04 00:29:20 -0500

    More Assets
    Added a map and more assets to use.

---
+++ Assets/Terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Terrain Tiles" tilewidth="32" tileheight="32" tilecount="117" columns="9">
+ <image source="dg_features32.png" width="288" height="416"/>
+</tileset>
