f3587331029d189b3af00b0e3c29a7ac4c1e39e8
Scaredturtle <demarcam2012@yahoo.com>
2017-12-24 17:17:00 -0500

    hero can't walk off screen
    waiting for you to text me back before I start doing random creep generation.... assuming you already did these things but didn't get around to pushing it all last night.

--- Component/movement/keyboardMovement.lua
+++ Component/movement/keyboardMovement.lua
@@ -3,12 +3,25 @@ local keyboardMovement = {}
 function keyboardMovement:update (entity)
     if love.keyboard.isDown("right") then
         entity.x = entity.x + entity.speed
+        if entity.x >= 785 then 
+            entity.x = -15
+            --random spawn of enemies here?
+        end
     elseif love.keyboard.isDown("left") then
         entity.x = entity.x - entity.speed
+        if entity.x <= -15 then
+            entity.x = 785
+        end
     elseif love.keyboard.isDown ("up") then
         entity.y = entity.y - entity.speed
+        if entity.y <= -15 then
+            entity.y = 585
+        end
     elseif love.keyboard.isDown("down") then
         entity.y = entity.y + entity.speed
+        if entity.y >= 585 then
+            entity.y = -15
+        end
     end
 end
 
