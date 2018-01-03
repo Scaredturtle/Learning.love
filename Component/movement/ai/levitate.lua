926a6d86079f3c0208e7ea3ad625a3f70974db69
demarchet <demarchet@yahoo.com>
2018-01-02 23:10:16 -0500

    forgot to turn ghost back on
    whoops. he died for a moment

--- Component/movement/ai/levitate.lua
+++ Component/movement/ai/levitate.lua
@@ -7,7 +7,7 @@ function levitate:update (entity)
     local aggro = false
     local moving = false
 
-    --[[ 
+
     if aggro == false then
         if dx < 10 and dy == 0 then
             entity.x = entity.x + entity.speed
@@ -26,7 +26,7 @@ function levitate:update (entity)
             dy = dy - 1
         end
     end
-    ]]--
+
 
     --these won't print unless moved to the draw function right?
     --which is invoked in update when ran on the entity right?
