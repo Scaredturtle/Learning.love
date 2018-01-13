9dfbbd963e221387989668e5d327ecf72eafab52
Mr. Fix-it <demarchet@yahoo.com>
2018-01-12 19:59:26 -0500

    added vertical looping
    looks like things are coming along here. quick update. more to come!

--- Component/movement/keyboardMovement.lua
+++ Component/movement/keyboardMovement.lua
@@ -3,27 +3,66 @@ local keyboardMovement = {}
 function keyboardMovement:update (entity)
     --if you handle the diagonals first it works better.
     --when going diagonal moving at %75 speed in each direction
+    --stops player from going off screen vertically. loop method
     if love.keyboard.isDown("right") and love.keyboard.isDown("down") then
         entity.x = entity.x + (entity.speed*.75)
-        entity.y = entity.y + (entity.speed*.75)
+        if entity.y + (entity.speed*.75) > 580 then
+            entity.y = 10
+        else
+            entity.y = entity.y + (entity.speed*.75)
+        end
+
+
     elseif love.keyboard.isDown("right") and love.keyboard.isDown("up") then
         entity.x = entity.x + (entity.speed*.75)
-        entity.y = entity.y - (entity.speed*.75)
+        if entity.y - (entity.speed*.75) < 10 then
+            entity.y = 580
+        else
+            entity.y = entity.y - (entity.speed*.75)
+        end
+
+
     elseif love.keyboard.isDown("left") and love.keyboard.isDown("down") then
         entity.x = entity.x - (entity.speed*.75)
-        entity.y = entity.y + (entity.speed*.75)
+        if entity.y + (entity.speed*.75) > 570 then
+            entity.y = 10
+        else
+            entity.y = entity.y + (entity.speed*.75)
+        end
+
+
     elseif love.keyboard.isDown("left") and love.keyboard.isDown("up") then
         entity.x = entity.x - (entity.speed*.75)
-        entity.y = entity.y - (entity.speed*.75)
+        if entity.y + (entity.speed*.75) < 10 then
+            entity.y = 580
+        else
+            entity.y = entity.y - (entity.speed*.75)
+        end
+
+
     elseif love.keyboard.isDown("right") then
         entity.x = entity.x + entity.speed
+
+
     elseif love.keyboard.isDown("left") then
         entity.x = entity.x - entity.speed
-    elseif love.keyboard.isDown ("up") then
-        entity.y = entity.y - entity.speed
+
+
     elseif love.keyboard.isDown("down") then
-        entity.y = entity.y + entity.speed
-    end
+        if entity.y + entity.speed > 580 then
+            entity.y = 10
+        else
+            entity.y = entity.y + entity.speed
+        end
+
+
+    elseif love.keyboard.isDown ("up") then
+        if entity.y - entity.speed < 10 then
+            entity.y = 580
+        else
+            entity.y = entity.y - entity.speed
+        end
+    end 
 end
 
 return keyboardMovement
